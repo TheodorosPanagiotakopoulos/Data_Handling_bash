44bca412e36e4c2ad9cf5f93f3ba04e93b389c19
thodorisP <teosfp@hotmail.com>
2025-04-09 04:46:00 -0400

    commit

--- continue_normal_job.sh
+++ continue_normal_job.sh
@@ -5,6 +5,7 @@ continue_MD() {
         echo "CONTCAR not found. Nothing to do."
         return
     fi
+
     num=$(( $(ls -d RUN*/ 2>/dev/null | wc -l) + 1 ))
     dir="RUN$num"
     mkdir "$dir"
